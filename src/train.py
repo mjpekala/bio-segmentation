5867ed0d9d598d77ea367d489465b41226a376e0
mjp <mike.pekala@jhuapl.edu>
2016-02-15 15:30:08 -0500

    now loads data volumes

--- src/train.py
+++ src/train.py
@@ -9,9 +9,11 @@ __license__ = "Apache 2.0"
 
 import sys, os.path
 import argparse
+import logging
 
 from keras.optimizers import SGD
 
+import emlib
 import models as emm
 
 
@@ -44,6 +46,10 @@ def _train_mode_args():
     parser.add_argument('--omit-labels', dest='omitLabels', 
 		    type=str, default='', 
 		    help='(optional) list of class labels to omit from training')
+    
+    parser.add_argument('--n-epochs', dest='nEpochs', 
+		    type=int, default=30,
+		    help='number of training epochs')
 
     parser.add_argument('--gpu', dest='gpu', 
 		    type=int, default=-1, 
@@ -69,12 +75,44 @@ def _train_mode_args():
 
 
 
+def _train_one_epoch(model, Xtrain, Ytrain):
+    pass
+    
+
+
 if __name__ == "__main__":
     args = _train_mode_args()
-    print args # TEMP
     
+    # whoever designed the logging module must have been a java programmer...
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter('[%(asctime)s:%(name)s:%(levelname)s]  %(message)s'))
+    logger.addHandler(ch)
+
+    # load training and validation volumes
+    Xtrain = emlib.load_cube(args.emTrainFile)
+    Ytrain = emlib.load_cube(args.labelsTrainFile)
+    if args.trainSlices:
+        Xtrain = Xtrain[args.trainSlices,:,:]
+        Ytrain = Ytrain[args.trainSlices,:,:]
+        
+    Xvalid = emlib.load_cube(args.emValidFile)
+    Yvalid = emlib.load_cube(args.labelsValidFile)
+    if args.validSlices:
+        Xvalid = Xvalid[args.validSlices,:,:]
+        Yvalid = Yvalid[args.validSlices,:,:]
+
+    logger.info('training volume dimensions:   %s' % str(Xtrain.shape))
+    logger.info('validation volume dimensions: %s' % str(Xvalid.shape))
+
+    # create and configure CNN
+    # XXX: make configurable via command line?
     model = emm.ciresan_n3()
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy', optimizer=sgd)
+
+    for epoch in range(args.nEpochs):
+        logger.info('starting training epoch %d' % epoch);
     
-    #model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
+        #model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
