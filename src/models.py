e364fb4a15fcda9da0b8bccec95d3930ca5afab1
mjp <mike.pekala@jhuapl.edu>
2016-02-15 14:36:56 -0500

    moved learning code into train.py

--- src/models.py
+++ src/models.py
@@ -8,7 +8,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-from keras.optimizers import SGD
 
 
 
--- src/train.py
+++ src/train.py
@@ -1,8 +1,12 @@
 """
 """
 
-__author__ = "mjp"
+__author__ = "Mike Pekala"
+__copyright__ = "Copyright 2016, JHU/APL"
+__license__ = "Apache 2.0"
 
+
+from keras.optimizers import SGD
 
 import models as emm
 
@@ -10,3 +14,7 @@ import models as emm
 
 if __name__ == "__main__":
     model = emm.ciresan_n3()
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer=sgd)
+    
+    #model.fit(X_train, Y_train, batch_size=32, nb_epoch=1)
